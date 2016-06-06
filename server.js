fc646852c14376c202a55650219ee4c053bdaf26
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-06-06 10:50:38 +0300

    init test commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var app = express();
+app.use((req, resp)=>{
+	req.send(
+		`  <div class="hello">
+		    <h1>hi all</h1>
+		    <p>olololoo</p>
+		  </div>
+		`
+	);
+});
