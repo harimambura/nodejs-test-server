181e22f65b23b6dc9d77f47d61b2bdbf3f210528
andrey-zhiryakov <andrey.zhiriakov@gmail.com>
2016-06-06 11:13:22 +0300

    fix server

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 var app = express();
-app.use('/',(req, resp)=>{
-	req.send(
+app.use('/exit',(req, res, next)=>{
+	res.send(
+		`  <div class="hello">
+		<h1>Command accepted!</h1>
+		<p>Shut down...</p>
+		</div>
+		`
+	);
+
+});
+app.use('/',(req, res, next)=>{
+	res.send(
 		`  <div class="hello">
 		    <h1>hi all</h1>
 		    <p>olololoo</p>
 		  </div>
 		`
 	);
+	next();
 });
+
+
+app.listen(8080);
